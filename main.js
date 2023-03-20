076dbb855d1c652d9579251a3cdb282df6c51f44
TrueVale <srvleh@gmail.com>
2023-03-20 12:59:40 +0100

    [DEV-0002] Initial configuration js + html + css

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 let input = document.querySelector('#file-input');
 let player = document.querySelector('#audio-player');
 
+let songListElement = document.querySelector("#song-list")
+
+
 if (input) {
-    input.addEventListener("change", handleAudio, false);
+    input.addEventListener("change", handleTemplate, false);
 }
 
-function handleAudio() {
-    Array.from(input.files).forEach(file => {
-        console.log(file.name)
-        player.src = URL.createObjectURL(file);
-    });
+function handleTemplate() {
+    const songItem = Array.from(input.files).map((file, index) => {
+        let audio = new Audio();
+        audio.src = URL.createObjectURL(file);
+        audio.onloadedmetadata = function() {
+            console.log(audio.duration)
+        };
+
+        return [
+            `
+            <div class="song-item">
+                <button class="play-btn" onclick="document.getElementById('audio-player${index}').play()"></button>
+                <p class="song-title">${ file.name }</p>
+                <audio id="audio-player${index}" controls="controls" preload="metadata" src="${ URL.createObjectURL(file) }"></audio>
+            </div>
+        `
+        ]
+    })
+
+    // Remove comma so won't show on template
+    let songItemModified = songItem.join("");
+
+    songListElement.innerHTML = songItemModified
 }
