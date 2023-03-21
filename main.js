8a1512d4a69ed444a0c1e0161d5fd1b9da285e4b
TrueVale <srvleh@gmail.com>
2023-03-21 15:54:20 +0100

    [DEV-0004] Add volume listeners

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ function handleTemplate() {
                 <button id="pause-btn${index}" class="btn pause-btn"></button>
                 <p class="song-title">${ file.name }</p>
                 <div class="volume-container">
-                    <button class="volume vol-plus" onclick="document.getElementById('audio-player${index}').volume+=.1"></button>
-                    <button class="volume vol-minus" onclick="document.getElementById('audio-player${index}').volume-=.1"></button>
+                    <button id="volume-up${index}" class="volume vol-plus"></button>
+                    <button id="volume-down${index}" class="volume vol-minus" onclick="document.getElementById('audio-player${index}').volume-=.1"></button>
                 </div>
                 <p id="totalTime${index}" class="song-duration"></p>
                 <div id="wave-anim${index}" class="boxContainer hide">
@@ -35,6 +35,25 @@ function handleTemplate() {
     addListeners()
 }
 
+function formatAndAssignToHTML(item, parent) {
+    let modifiedItem = item.join("");
+    parent.innerHTML = modifiedItem
+}
+
+function getTotalTime() {
+    songItem.forEach((song, index) => {
+        let audioPlayer = document.getElementById(`audio-player${index}`)
+        let totalTime = document.getElementById(`totalTime${index}`)
+
+        audioPlayer.onloadedmetadata = function() {
+            const minutes = parseInt(Math.floor(audioPlayer.duration / 60));
+            const seconds = parseInt(audioPlayer.duration - minutes * 60);
+
+            totalTime.innerText = `${minutes}:${seconds}`
+        };
+    })
+}
+
 function addListeners() {
     Array.from(input.files).forEach((file, index) => {
         let playBtn = document.getElementById(`play-btn${index}`)
@@ -45,6 +64,16 @@ function addListeners() {
         let pauseBtn = document.getElementById(`pause-btn${index}`)
         pauseBtn.addEventListener("click", () => {
             pauseClicked(index)
+        })
+
+        let volumeUp = document.getElementById(`volume-up${index}`)
+        volumeUp.addEventListener("click", () => {
+            document.getElementById(`audio-player${index}`).volume+=.1
+        })
+
+        let volumeDown = document.getElementById(`volume-down${index}`)
+        volumeDown.addEventListener("click", () => {
+            document.getElementById(`audio-player${index}`).volume-=.1
         })
     })
 }
@@ -58,25 +87,3 @@ function pauseClicked(id){
     document.getElementById(`audio-player${id}`).pause()
     document.getElementById(`wave-anim${id}`).classList.toggle('hide')
 }
-
-function formatAndAssignToHTML(item, parent) {
-
-    let modifiedItem = item.join("");
-
-    parent.innerHTML = modifiedItem
-}
-
-function getTotalTime() {
-    songItem.forEach((song, index) => {
-        let audioPlayer = document.getElementById("audio-player"+index)
-        let totalTime = document.getElementById("totalTime"+index)
-        audioPlayer.onloadedmetadata = function() {
-            const minutes = parseInt(Math.floor(audioPlayer.duration / 60));
-            const seconds = parseInt(audioPlayer.duration - minutes * 60);
-            totalTime.innerText = `${minutes}:${seconds}`
-        };
-    })
-}
-
-/*
-*/
