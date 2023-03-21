926a9eb3e3591ede4376a4dc41bd02b72fd1fe5e
TrueVale <srvleh@gmail.com>
2023-03-21 14:04:24 +0100

    [DEV-0003] Template creation logic and styles

--- main.js
+++ main.js
@@ -1,34 +1,82 @@
 let input = document.querySelector('#file-input');
-let player = document.querySelector('#audio-player');
-
-let songListElement = document.querySelector("#song-list")
-
+let songListElement = document.querySelector("#song-list");
+let songItem;
 
 if (input) {
     input.addEventListener("change", handleTemplate, false);
 }
 
 function handleTemplate() {
-    const songItem = Array.from(input.files).map((file, index) => {
-        let audio = new Audio();
-        audio.src = URL.createObjectURL(file);
-        audio.onloadedmetadata = function() {
-            console.log(audio.duration)
-        };
-
-        return [
-            `
+    songItem = Array.from(input.files).map((file, index) => {
+        return [`
             <div class="song-item">
-                <button class="play-btn" onclick="document.getElementById('audio-player${index}').play()"></button>
+                <button id="play-btn${index}" class="btn play-btn"></button>
+                <button id="pause-btn${index}" class="btn pause-btn"></button>
                 <p class="song-title">${ file.name }</p>
-                <audio id="audio-player${index}" controls="controls" preload="metadata" src="${ URL.createObjectURL(file) }"></audio>
+                <div class="volume-container">
+                    <button class="volume vol-plus" onclick="document.getElementById('audio-player${index}').volume+=.1"></button>
+                    <button class="volume vol-minus" onclick="document.getElementById('audio-player${index}').volume-=.1"></button>
+                </div>
+                <p id="totalTime${index}" class="song-duration"></p>
+                <div id="wave-anim${index}" class="boxContainer hide">
+                  <div class="box box1"></div>
+                  <div class="box box2"></div>
+                  <div class="box box3"></div>
+                  <div class="box box4"></div>
+                  <div class="box box5"></div>
+                </div>
+                <audio id="audio-player${index}"  preload="metadata" src="${ URL.createObjectURL(file) }"></audio>
             </div>
-        `
-        ]
+        `]
     })
 
-    // Remove comma so won't show on template
-    let songItemModified = songItem.join("");
-
-    songListElement.innerHTML = songItemModified
+    formatAndAssignToHTML(songItem, songListElement);
+    getTotalTime()
+    addListeners()
 }
+
+function addListeners() {
+    Array.from(input.files).forEach((file, index) => {
+        let playBtn = document.getElementById(`play-btn${index}`)
+        playBtn.addEventListener("click", () => {
+            playClicked(index)
+        })
+
+        let pauseBtn = document.getElementById(`pause-btn${index}`)
+        pauseBtn.addEventListener("click", () => {
+            pauseClicked(index)
+        })
+    })
+}
+
+function playClicked(id){
+    document.getElementById(`audio-player${id}`).play()
+    document.getElementById(`wave-anim${id}`).classList.toggle('hide')
+}
+
+function pauseClicked(id){
+    document.getElementById(`audio-player${id}`).pause()
+    document.getElementById(`wave-anim${id}`).classList.toggle('hide')
+}
+
+function formatAndAssignToHTML(item, parent) {
+
+    let modifiedItem = item.join("");
+
+    parent.innerHTML = modifiedItem
+}
+
+function getTotalTime() {
+    songItem.forEach((song, index) => {
+        let audioPlayer = document.getElementById("audio-player"+index)
+        let totalTime = document.getElementById("totalTime"+index)
+        audioPlayer.onloadedmetadata = function() {
+            const minutes = parseInt(Math.floor(audioPlayer.duration / 60));
+            const seconds = parseInt(audioPlayer.duration - minutes * 60);
+            totalTime.innerText = `${minutes}:${seconds}`
+        };
+    })
+}
+
+/*
+*/
